e1e2292813545e70849ec83e8df9c1a053779bbc
Francis Jumawan <69629153+sherkaicode@users.noreply.github.com>
2025-04-24 17:04:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
+from fh_matplotlib import matplotlib2fasthtml
 from fasthtml.common import *
+import numpy as np
+import matplotlib.pylab as plt
 
-app, rt = fast_app(hdrs=(picolink))
+app, rt = fast_app()
 
+@matplotlib2fasthtml
+def generate_chart(num_points):
+    plotdata = [np.random.exponential(1) for _ in range(num_points)]
+    plt.plot(range(len(plotdata)), plotdata)
 
-@rt("/")
-def get():
-    return (
-        Socials(
-            title="Vercel + FastHTML",
-            site_name="Vercel",
-            description="A demo of Vercel and FastHTML integration",
-            image="https://vercel.fyi/fasthtml-og",
-            url="https://fasthtml-template.vercel.app",
-            twitter_site="@vercel",
-        ),
-        Container(
-            Card(
-                Group(
-                    P(
-                        "ABCDEFGHIJKLMNOP Test",
-                    ),
-                ),
-                header=(Titled("A.E.G.I.S.: Sherkaicode Website")),
-                footer=(
-                    P(
-                        A(
-                            "Deploy your own",
-                            href="https://vercel.com/templates/python/fasthtml-python-boilerplate",
-                        ),
-                        " or ",
-                        A("learn more", href="https://docs.fastht.ml/"),
-                        "about FastHTML.",
-                    )
-                ),
-            ),
-        ),
+@rt
+def index():
+    return Div(
+        Div(id="chart"),
+        H3("Move the slider to change the graph"),
+        Input(
+            type="range",
+            min="1", max="10", value="1",
+            get=update_chart, hx_target="#chart",
+            name='slider')
     )
 
-
-serve()
+@rt
+def update_chart(slider: int):
+    return Div(
+        generate_chart(slider),
+        P(f"Number of data points: {slider}")
+    )
